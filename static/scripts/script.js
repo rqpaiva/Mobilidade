53c0b0ef8cd5df895d556d01fafeb1bd218c9630
rqpaiva <144071319+rqpaiva@users.noreply.github.com>
2024-11-07 19:14:17 -0300

    Update script.js

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,39 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Função genérica para uploads
-    async function handleUpload(event, formId, actionUrl, successMessage) {
-        event.preventDefault();
-        const form = document.getElementById(formId);
-        const formData = new FormData(form);
+document.addEventListener('DOMContentLoaded', function() {
+    const fileInput = document.getElementById('fileInput');
+    const fileLabel = document.querySelector('.custom-file-label');
+    const textUpload = document.querySelector('.text-upload');
+    const buttonSubimit = document.querySelector('.buttonSubimit');
 
-        try {
-            const response = await fetch(actionUrl, {
-                method: 'POST',
-                body: formData,
-            });
+    
+    fileInput.addEventListener('change', function() {
+        if (fileInput.files.length > 0) {
+            textUpload.textContent = fileInput.files[0].name;
+            fileLabel.classList.add('hidenButton');
+            buttonSubimit.classList.remove('hidenButton')
 
-            const result = await response.json();
-            if (response.ok) {
-                alert(successMessage);
-                location.reload(); // Recarregar para ver atualizações
-            } else {
-                alert(result.error || 'Erro no upload do arquivo.');
-            }
-        } catch (error) {
-            console.error(`Erro ao enviar ${formId}:`, error);
-            alert('Erro ao conectar ao servidor.');
+        } else {
+            fileLabel.textContent = 'Nenhum arquivo selecionado';
         }
-    }
-
-    // Gerenciar uploads
-    document.getElementById('uploadCSVForm').addEventListener('submit', (event) =>
-        handleUpload(event, 'uploadCSVForm', '/upload', 'CSV carregado com sucesso!')
-    );
-
-    document.getElementById('uploadFavelasForm').addEventListener('submit', (event) =>
-        handleUpload(event, 'uploadFavelasForm', '/upload_favelas', 'GeoJSON de Favelas atualizado com sucesso!')
-    );
-
-    document.getElementById('uploadCensoForm').addEventListener('submit', (event) =>
-        handleUpload(event, 'uploadCensoForm', '/upload_censo', 'GeoJSON do Censo atualizado com sucesso!')
-    );
+    });
 });
