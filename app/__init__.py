2f71ff5b32bafc23149164434584dc90171a1ff2
rqpaiva <144071319+rqpaiva@users.noreply.github.com>
2025-01-29 16:48:04 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,32 +1,30 @@
-from flask import Flask, request
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import geopandas as gpd
-import networkx as nx
+from flask import Flask
 from flask_session import Session
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from collections import Counter
-from tabulate import tabulate
 from flask_cors import CORS
-import io
+from pymongo import MongoClient
+import os
 import secrets
-import base64
-import json
-import gmaps
-from flask_pymongo import PyMongo
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "Q9WE9QWSALKDIOASDU092U3EKDFWF"
-app.config["MONGO_URI"] = "mongodb+srv://felipepaivadev:Kv8TkdhEdR9ldeYn@cluster0.qzl2q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
+# Configurações gerais do Flask
+app = Flask(__name__)
+CORS(app)
+
+# Configurações de upload
+UPLOAD_FOLDER = 'upload_files/'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Configurações de sessão
 app.config['SESSION_TYPE'] = 'filesystem'
-app.config['SESSION_FILE_DIR'] = './flask_session'  # Diretório onde as sessões serão salvas
+app.config['SESSION_FILE_DIR'] = './flask_session'
 app.config['SESSION_PERMANENT'] = False
 app.config['SECRET_KEY'] = secrets.token_hex(16)
 Session(app)
 
+# Configuração do MongoDB
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
+db = client["mobility_data"]
 
-
-from app.views import home
+# Importar rotas do arquivo views.py
+from app.views import *
