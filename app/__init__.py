6b55ae35e6b81755cf7d499aa5bc1ee0f85e4ca9
rqpaiva <144071319+rqpaiva@users.noreply.github.com>
2025-01-30 15:47:27 -0300

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_session import Session
+from flask_cors import CORS
+from pymongo import MongoClient
+import os
+import secrets
+
+# Configurações gerais do Flask
+app = Flask(__name__)
+CORS(app)
+
+# Configurações de upload
+UPLOAD_FOLDER = 'upload_files/'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Configurações de sessão
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = './flask_session'
+app.config['SESSION_PERMANENT'] = False
+app.config['SECRET_KEY'] = secrets.token_hex(16)
+Session(app)
+
+# Configuração do MongoDB
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
+db = client["mobility_data"]
+
+# Importar rotas do arquivo views.py
+from app.views import *
